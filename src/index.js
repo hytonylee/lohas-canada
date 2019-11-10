6409c90e26b5e06e60badb5a07cac6e265de7985
Tony Lee <hy.tony.lee@gmail.com>
2019-11-09 19:13:07 -0800

    Add post router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!!');
+let express = require('express');
+let app = express();
+
+let postRoute = require('./routes/post');
+
+app.use(postRoute);
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
---
+++ src/routes/post.js
@@ -0,0 +1,19 @@
+let express = require('express');
+let router = express.Router();
+
+// Querytring => query property on the request object
+// locathost:4000/post?title=tomas&category=product
+router.get('/post/', (req, res) => {
+	if (req.query.title) {
+		res.send(`You have requst a post ${req.query.title}`);
+	} else {
+		res.send('You have request a post.');
+	}
+});
+
+// Params property on the request object
+router.get('/post/:title', (req, res) => {
+	res.send(`You have a request a post ${req.params.title}`);
+});
+
+module.exports = router;
