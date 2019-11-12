64131322ed6baac6a33fab2fb1d5871ae20d6ff7
Tony Lee <hy.tony.lee@gmail.com>
2019-11-11 20:10:43 -0800

    Server status 500 with separate path from public folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 let postRoute = require('./routes/post');
+let path = require('path');
 
 // middleware
 app.use((req, res, next) => {
@@ -10,6 +11,14 @@ app.use((req, res, next) => {
 });
 app.use(postRoute);
 app.use(express.static('public'));
+
+// handle for 404
+app.use((req, res, next) => {
+	res.status(404).send('We think we are lost!');
+});
+
+// handle for 500
+app.use((err, req, res, next) => {});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
