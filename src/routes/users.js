f0637f44624040ebec5a9db6020e57ff231b8a6b
Tony Lee <hy.tony.lee@gmail.com>
2019-11-22 22:05:49 -0800

    Omit new user login info'

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,29 +1,40 @@
 import express from 'express';
 import User from '../models/user.model';
 import { validateUser, validateLogin } from '../validation/validate';
+import bcrypt from 'bcryptjs';
 
 const router = express.Router();
 
 // Create a user
-router.route('/add').post((req, res) => {
+router.post('/add', async (req, res) => {
 	// Validate the data before create a user
 	const { error } = validateUser(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
 	// Check if the email exists
-	const emailExist = User.find({ email: req.body.email });
+	const emailExist = await User.findOne({
+		email: req.body.email
+	});
 	if (emailExist) return res.status(400).send('Email is already existed!');
 
-	const username = req.body.username;
-	const email = req.body.email;
-	const password = req.body.password;
+	// // Hash Password
+	const salt = await bcrypt.genSalt(10);
+	const hashPassword = await bcrypt.hash(req.body.password, salt);
 
 	//Create a User
-	const newUser = new User({ username, email, password });
-	newUser
-		.save()
-		.then(() => res.json('User added!'))
-		.catch(err => res.status(400).json('Error: ' + err));
+	const newUser = new User({
+		username: req.body.username,
+		email: req.body.email,
+		password: hashPassword
+	});
+
+	try {
+		const saveUser = await newUser.save();
+		// res.send(saveUser);
+		res.send({ user: saveUser._id });
+	} catch (err) {
+		res.status(400).send('Error: ' + err);
+	}
 });
 
 // Find a User
@@ -36,7 +47,7 @@ router.route('/:id').get((req, res) => {
 // Get All User
 router.route('/').get((req, res) => {
 	User.find()
-		.then(users => res.json(users))
+		.then(users => res.send(users))
 		.catch(err => res.status(400).json('Error: ' + err));
 });
 
