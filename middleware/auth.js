199c2efcdae61b38ef90586bc00875fd95c0fe32
Tony Lee <hy.tony.lee@gmail.com>
2020-01-14 17:23:23 -0800

    Create route and model for register user

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+	const token = req.header('x-auth-token');
+
+	// Check if not token
+	if (!token) {
+		return res.status(401).json({
+			msg: 'Missing Token: Authorization denied!'
+		});
+	}
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+	} catch (err) {
+		res.status(401).json({ msg: 'Invalid Token: Access denied!' });
+	}
+};
--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 
-// connect to Database
+// Connect to Database
 connectDB();
 
 // Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) =>
 	res.json({ msg: 'Welcome to the Lohas Farm Canada API...' })
 );
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/posts', require('./routes/posts'));
 
 // Connect to Port
 const PORT = process.env.PORT || 4000;
