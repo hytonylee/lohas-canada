125d9917ffd6faa3de6b45e9d8bd78492fd2fe3f
Tony Lee <hy.tony.lee@gmail.com>
2020-01-24 16:14:34 -0800

    fix footer alignment

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -2,39 +2,41 @@ import React from 'react';
 
 const Login = () => {
 	return (
-		<div className='form-container'>
-			<h1>
-				<span className='text-primary'>Dashboard Login</span>
-			</h1>
-			<form>
-				<div className='form-group'>
-					<label htmlFor='email'>Email Address</label>
+		<div className='container p-1'>
+			<div className='form-container'>
+				<h1>
+					<span className='text-primary'>Dashboard Login</span>
+				</h1>
+				<form>
+					<div className='form-group'>
+						<label htmlFor='email'>Email Address</label>
+						<input
+							type='email'
+							name='email'
+							// value={email}
+							autoComplete='email'
+							onChange='#'
+							required
+						/>
+					</div>
+					<div className='form-group'>
+						<label htmlFor='password'>Password</label>
+						<input
+							type='password'
+							name='password'
+							// value={password}
+							autoComplete='password'
+							onChange='#'
+							required
+						/>
+					</div>
 					<input
-						type='email'
-						name='email'
-						// value={email}
-						autoComplete='email'
-						onChange='#'
-						required
+						type='submit'
+						value='Login'
+						className='btn btn-primary btn-block'
 					/>
-				</div>
-				<div className='form-group'>
-					<label htmlFor='password'>Password</label>
-					<input
-						type='password'
-						name='password'
-						// value={password}
-						autoComplete='password'
-						onChange='#'
-						required
-					/>
-				</div>
-				<input
-					type='submit'
-					value='Login'
-					className='btn btn-primary btn-block'
-				/>
-			</form>
+				</form>
+			</div>
 		</div>
 	);
 };
