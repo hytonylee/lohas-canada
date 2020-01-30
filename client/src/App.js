6f681a56cf619322e4a2cc9e38f18fb10ba4b181
Tony Lee <hy.tony.lee@gmail.com>
2020-01-29 17:12:32 -0800

    Change style
    Change body background, card background, and add font awesome icons

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Contact from './components/pages/Contact';
 import Product from './components/pages/Product';
 import Blog from './components/pages/Blog';
 import Shop from './components/pages/Shop';
+import Dashboard from './components/pages/Dashboard';
 import Login from './components/auth/Login';
 import PrivateRoute from './components/routing/PrivateRoute';
 
@@ -37,6 +38,7 @@ const App = () => {
 							<Route exact path='/product' component={Product} />
 							<Route exact path='/blog' component={Blog} />
 							<Route exact path='/contact' component={Contact} />
+							<Route exact path='/dashboard' component={Dashboard} />
 							<Route exact path='/shop' component={Shop} />
 							<Route exact path='/login' component={Login} />
 						</Switch>
--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -13,10 +13,15 @@ const NavBar = () => {
 				/>
 			</h1>
 			<ul>
-				<Link to='/'>Home</Link>
+				<Link to='/'>
+					<i className='fas fa-home'></i>
+				</Link>
 				<Link to='/product'>Product</Link>
 				<Link to='/blog'>Blog</Link>
 				<Link to='/contact'>Contact</Link>
+				<Link to='/contact'>
+					<i className='fas fa-tools'></i>
+				</Link>
 				<Link to='/shop'>
 					<li>
 						<i className='fas fa-shopping-cart'></i>
