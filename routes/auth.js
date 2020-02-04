bb9501d21043afe0e7552d013c04b35d8c6b0053
Tony Lee <hy.tony.lee@gmail.com>
2020-02-03 19:14:32 -0800

    Create PricterRoute Component

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ const User = require('../models/User');
 router.get('/', auth, async (req, res) => {
 	try {
 		const user = await User.findById(req.user.id).select('-password');
-		res.json(user);
+		res.json(user).send('Logged In Success!');
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server Error!');
@@ -41,13 +41,13 @@ router.post(
 			let user = await User.findOne({ email });
 
 			if (!user) {
-				return res.status(400).json({ msg: 'Invalid Credentilas!' });
+				return res.status(400).json({ msg: 'Invalid Credentials!' });
 			}
 
 			const isMatch = await bcrypt.compare(password, user.password);
 
 			if (!isMatch) {
-				return res.status(400).json({ msg: 'Invalid Credentilas!' });
+				return res.status(400).json({ msg: 'Invalid Credentials!' });
 			}
 
 			const payload = {
