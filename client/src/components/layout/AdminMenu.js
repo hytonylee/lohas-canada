6b04814be3cde86cd13e7b166615490ef3b0a41e
Tony Lee <hy.tony.lee@gmail.com>
2020-02-07 16:48:58 -0800

    Change AdminMenu layout

--- client/src/components/layout/AdminMenu.js
+++ client/src/components/layout/AdminMenu.js
@@ -7,9 +7,12 @@ const AdminMenu = () => {
 
 	return (
 		<div style={{ backgroundColor: '#3f3e3e', height: '30px', color: 'white' }}>
-			<div className='container'>
+			<div className='sub-menu'>
 				<ul>
 					<li>Hello, {user && user.name}</li>
+					<li>
+						<i className='fas fa-plus-circle'></i>
+					</li>
 				</ul>
 			</div>
 		</div>
