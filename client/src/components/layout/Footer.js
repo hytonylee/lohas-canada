4aa182d368a205b5129020eedf7f827976c30794
Tony Lee <hy.tony.lee@gmail.com>
2020-02-10 15:37:35 -0800

    Edit footer style

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,9 +3,15 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<div className='footer bg-dark' style={{ fontSize: '12px' }}>
-			<ul>
-				<li>Copyright @ Lohas Farm 2019</li>
-			</ul>
+			<div className='container'>
+				<ul>
+					<li>Copyright @ Lohas Farm 2019</li>
+					<li> | </li>
+					<li>Site Map</li>
+					<li> | </li>
+					<li>Contact</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
