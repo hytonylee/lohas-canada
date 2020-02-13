7bc743017f43a948de3cc5c73ca04730c4df914d
Tony Lee <hy.tony.lee@gmail.com>
2020-02-13 15:32:16 -0800

    Add image to post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import defaultImage from './defaultImage.jpg';
 
 const PostItem = ({ post }) => {
-	const { _id, title, content } = post;
+	const { _id, title, content, imgUrl, date } = post;
 
 	return (
-		<div className='card' keys={_id}>
-			<h3>{title}</h3>
-			<p>{content}</p>
+		<div className='card-post' keys={_id}>
+			<img className='image' src={imgUrl ? imgUrl : defaultImage} />
+			<div className='card-container'>
+				<h3>{title}</h3>
+				<h6>{date}</h6>
+				<p>{content}</p>
+			</div>
 		</div>
 	);
 };
