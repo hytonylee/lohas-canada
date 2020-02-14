dcd3f8f1187ed6b2f325864ce0e530ef339b11aa
Tony Lee <hy.tony.lee@gmail.com>
2020-02-13 17:00:38 -0800

    Remove server greeting

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-	res.json({ msg: 'Welcome to the Lohas Farm Canada API...' })
-);
+// app.get('/', (req, res) =>
+// 	res.json({ msg: 'Welcome to the Lohas Farm Canada API...' })
+// );
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
