f7f04acf0c61982f6e0fd34c5b8caaff3db38651
Tony Lee <hy.tony.lee@gmail.com>
2020-02-13 16:47:22 -0800

    Add build file

---
+++ client/build/precache-manifest.fc643a415d0949ad4d4cd5b48fe8a9ce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c200d44b8f8f7bd382bf09d56377c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d07bed2ebfaf06195f1",
+    "url": "/static/css/main.95fb2f4b.chunk.css"
+  },
+  {
+    "revision": "967b9f97591c3af74169",
+    "url": "/static/js/2.e9f36edc.chunk.js"
+  },
+  {
+    "revision": "95c534deb415fe087260dab4786e3626",
+    "url": "/static/js/2.e9f36edc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d07bed2ebfaf06195f1",
+    "url": "/static/js/main.8e098878.chunk.js"
+  },
+  {
+    "revision": "ca2bad245ea328e43eb6",
+    "url": "/static/js/runtime-main.bac0a988.js"
+  },
+  {
+    "revision": "b956f2ced63980185a8920fc5b6c1ec8",
+    "url": "/static/media/bg_img.b956f2ce.jpg"
+  },
+  {
+    "revision": "bbded77b8f78e1e7955a59830dd6917b",
+    "url": "/static/media/defaultImage.bbded77b.jpg"
+  },
+  {
+    "revision": "f4404720ece11355df318a0acf525cb1",
+    "url": "/static/media/spinner.f4404720.gif"
+  }
+]);
