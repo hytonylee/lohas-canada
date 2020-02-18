f061a3f24a5b0e72cbfa66ad7b720fd5246943d8
Tony Lee <hy.tony.lee@gmail.com>
2020-02-17 22:48:53 -0800

    Change useState in Home and Blog' '

--- client/src/components/pages/Blog.js
+++ client/src/components/pages/Blog.js
@@ -1,13 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
+import PostContext from '../../context/post/postContext';
 import Display from '../layout/Display';
 import Posts from '../posts/Posts';
 
 const Blog = () => {
+	const postContext = useContext(PostContext);
+	const { section } = postContext;
+	useEffect(() => {
+		if (section !== 'blog') {
+			section = 'blog';
+		}
+	}, [postContext]);
 	return (
 		<Fragment>
 			<Display />
 			<div className='container'>
-				<Posts page='blog' />
+				<Posts />
 			</div>
 		</Fragment>
 	);
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,14 +1,22 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import PostContext from '../../context/post/postContext';
 import Display from '../layout/Display';
 import Posts from '../posts/Posts';
 
 const Home = () => {
+	const postContext = useContext(PostContext);
+	const { section } = postContext;
+	useEffect(() => {
+		if (section !== 'home') {
+			section = 'home';
+		}
+	}, [postContext]);
+
 	return (
 		<Fragment>
 			<Display />
 			<div className='container'>
-				<Posts section='home' />
+				<Posts />
 			</div>
 		</Fragment>
 	);
