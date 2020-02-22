ebcc2f4776ff250983081a8918c100cbc945af0f
Tony Lee <hy.tony.lee@gmail.com>
2020-02-22 13:00:04 -0800

    Change footer column size

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,15 +3,42 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<footer className='page-footer grey darken-3'>
-			<div className='container'>
+			<div className='footerWrapper'>
 				<div className='row'>
-					<div className='col l6 s12'>
-						<h5 className='white-text'>Footer Content</h5>
+					<div className='col l5 s12'>
+						<h5 className='white-text'>About Us</h5>
 						<p className='grey-text text-lighten-4'>
-							You can use rows and columns here to organize your footer content.
+							Lohas Farm Canada founded in 2008, and have been managing and
+							operating organic farms in both BC and PEI to produce best quality
+							blueberries and apples with the mission to protect the environment
+							and promoting healthy living.
 						</p>
 					</div>
-					<div className='col l4 offset-l2 s12'>
+					<div className='col l3'></div>
+					<div className='col l2 s12'>
+						<h5 className='white-text'>Our Partners</h5>
+						<p className='grey-text text-lighten-4'>
+							<ul>
+								<li>
+									<a
+										className='grey-text text-lighten-3'
+										href='http://www.leezen.com.tw/'
+									>
+										LeeZen Organic
+									</a>
+								</li>
+								<li>
+									<a
+										className='grey-text text-lighten-3'
+										href='https://gebis.org/'
+									>
+										GEBIS
+									</a>
+								</li>
+							</ul>
+						</p>
+					</div>
+					<div className='col l2 s12'>
 						<h5 className='white-text'>Links</h5>
 						<ul>
 							<li>
@@ -39,12 +66,7 @@ const Footer = () => {
 				</div>
 			</div>
 			<div className='footer-copyright'>
-				<div className='container'>
-					© 2014 Copyright Text
-					<a className='grey-text text-lighten-4 right' href='#!'>
-						More Links
-					</a>
-				</div>
+				<div className='footerWrapper'>© 2014 Copyright Text</div>
 			</div>
 		</footer>
 	);
