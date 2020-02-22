9fc42fc626051ab6e61799a9b45b7f4391ca2425
Tony Lee <hy.tony.lee@gmail.com>
2020-02-22 15:22:09 -0800

    Remove materialize-css for slider on Home.js

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,27 +1,8 @@
 import React, { Fragment, useEffect } from 'react';
 // import Display from '../layout/Display';
 import Posts from '../posts/Posts';
-import M from 'materialize-css/dist/js/materialize.min.js';
 
 const Home = () => {
-	useEffect(() => {
-		const instance = M.Slider.init(_slider.current, options);
-
-		if (activeIndex) {
-			if (typeof indicators === 'undefined' || options.indicators) {
-				instance['$indicators'][activeIndex].className =
-					'indicator-item active';
-			}
-		}
-
-		return () => {
-			if (instance) {
-				setActiveIndex(instance.activeIndex);
-				instance.destroy();
-			}
-		};
-	}, [_slider, options, fullscreen, activeIndex]);
-
 	return (
 		<Fragment>
 			{/* <Display /> */}
