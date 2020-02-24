9a818e5e0a8f5c52af49f98721b56b5176b4b96c
Tony Lee <hy.tony.lee@gmail.com>
2020-02-24 14:35:30 -0800

    Add modal for add post

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -6,10 +6,13 @@ import PostForm from '../posts/PostForm';
 import AddPostModal from '../posts/AddPostModal';
 import AuthContext from '../../context/auth/authContext';
 
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 const Dashboard = () => {
 	const authContext = useContext(AuthContext);
 	useEffect(() => {
 		authContext.loadUser();
+		M.AutoInit();
 		// eslint-disable-next-line
 	}, []);
 
@@ -17,6 +20,7 @@ const Dashboard = () => {
 		<div className='container'>
 			<AddBtn />
 			<PostFilter />
+			<AddPostModal />
 			<Posts postSection='dashboard' />
 		</div>
 	);
--- client/src/components/posts/AddPostModal.js
+++ client/src/components/posts/AddPostModal.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import M from 'materialize-css/dist/js/materialize.min.js';
+// import PropTypes from 'prop-types';
 
 const AddPostModal = () => {
-	return <div></div>;
+	return (
+		<div id='add-post-modal' className='modal'>
+			<div className='modal-content'>
+				<h4>Add A Post</h4>
+			</div>
+			<div className='row'>
+				<input type='text' name='title' />
+				<label htmlFor='title' className='active'>
+					Title
+				</label>
+			</div>
+			<div className='modal-footer'>
+				<a href='#!' className='modal-close waves-effect waves blue btn'>
+					Enter
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default AddPostModal;
