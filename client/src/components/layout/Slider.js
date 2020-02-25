bb9399f06c60195ad638ee1867a7e0fa7e1d12b8
Tony Lee <hy.tony.lee@gmail.com>
2020-02-25 13:39:08 -0800

    Fix slider display issue conitinue

--- client/src/components/layout/Slider.js
+++ client/src/components/layout/Slider.js
@@ -2,10 +2,24 @@ import React, { useEffect, useContext } from 'react';
 import PostContext from '../../context/post/postContext';
 import SlideItem from '../layout/SlideItem';
 import PreLoader from '../layout/PreLoader';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 const Slider = ({ slides }) => {
 	const postContext = useContext(PostContext);
 	const { loading } = postContext;
+
+	useEffect(() => {
+		let slider = document.querySelectorAll('.slider');
+		let options = {
+			indicators: true,
+			height: 250,
+			inDuration: 300,
+			outDuration: 300,
+			hover: true, // Activate on hover
+			coverTrigger: true // Displays dropdown below the button
+		};
+		M.Slider.init(slider, options);
+	}, []);
 	return (
 		<div className='slider slideWrapper'>
 			<ul className='slides'>
